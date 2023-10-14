9c0d95e6d24925c53b2f7cbc219b9d5c118cc1fa
grafst <grafst@users.noreply.github.com>
2023-10-14 12:37:13 +0200

    fix working dir & WIP

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
-sudo apt install uhubctl git python3-pip python3-flask
-pip3 install requirements.txt
+sudo apt install uhubctl git python3-pip python3-flask iptables
 sudo cp motd /etc/motd
-# redirect port 80 to 8080
+echo "cd /opt/radiosilence" | sudo tee -a ~/.bashrc
+echo "~~~ redirect requests on port 80 to 8080 ~~~~"
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+sudo apt install -y iptables-persistent
+echo "~~~ install service ~~~"
 sudo cp -f radiosilence.service /lib/systemd/system/radiosilence.service
 sudo chmod 644 /lib/systemd/system/radiosilence.service
 sudo systemctl daemon-reload
 sudo systemctl enable radiosilence.service
-echo "cd /opt/radiosilence" | sudo tee -a ~/.bashrc
 sudo service radiosilence start
