1f1b43f0b9a692cfa1db42fef69473ebb0ca9cea
grafst <grafst@users.noreply.github.com>
2023-10-14 12:32:02 +0200

    WIP

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-sudo apt install uhubctl git python3-pip
+sudo apt install uhubctl git python3-pip python3-flask
 pip3 install requirements.txt
 sudo cp motd /etc/motd
+# redirect port 80 to 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo cp -f radiosilence.service /lib/systemd/system/radiosilence.service
 sudo chmod 644 /lib/systemd/system/radiosilence.service
 sudo systemctl daemon-reload
