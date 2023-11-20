1f3e8174845bcb20535c0dceb7469916c6620552
grafst <grafst@users.noreply.github.com>
2023-11-20 17:00:33 +0100

    restore after phonecall

--- radiosilence.py
+++ radiosilence.py
@@ -9,18 +9,35 @@ def usb_on():
     os.system('sudo uhubctl -a on -l 1-1')
 
 if __name__ =="__main__":
+    state='loud'
+    previous_state='loud' # todo: read from file
     app = flask.Flask(__name__)
     @app.route('/silent')
     def silent():
         usb_off()
+        previous_state=state
+        state='silent'
         logging.info("Silent mode on")
         return 'Silent mode on'
     
     @app.route('/loud')
     def loud():
         usb_on()
+        previous_state=state
+        state='loud'
         logging.info("Silent mode off")
         return 'Silent mode off'
+    
+    @app.route('/restore')
+    def restore():
+        if previous_state=='loud':
+            usb_on()
+            logging.info("Restored to loud")
+            return "Restored to loud"
+        else:
+            logging.info("Keeping silent")
+            return "Keeping silent"
+        
     
     @app.route('/ip')
     def ip():
@@ -37,13 +54,13 @@ if __name__ =="__main__":
         import subprocess
         process = subprocess.Popen(['sudo', 'uhubctl', '-l', '1-1'], stdout=subprocess.PIPE)
         stdout = process.communicate()[0]
-        if 'Port 1 status: power off' in stdout.decode('utf-8'):
-            return 'Silent mode on'
+        if 'off' in stdout.decode('utf-8'):
+            return 'silent'
         else:
-            return 'Silent mode off'
-
+            return 'loud'
         
     
     # start flask app
     app.run(host='0.0.0.0', port=8080, debug=False)
-
+    
+    
